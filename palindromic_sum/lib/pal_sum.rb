275879a969d98da52c8313bae2cca27800c0c2dd
Garrett Gregor <garrett.gregor@gmail.com>
2023-09-14 14:51:40 -0600

    feat: palindrome sums

---
+++ palindromic_sum/lib/pal_sum.rb
@@ -0,0 +1,28 @@
+class PalSum
+  # generate all palindromes between 0-1000
+  # create nested arrays of the palindromes
+  # add the two numbers together
+  # store that in the array as a third element
+  # check to see if the third is element is greater or equal to 1000
+  # if it is, shovel it into the output array
+  # return the output array with a limit of 25 integers
+  def first_twenty_five
+    nums = Array(0..1000)
+
+    palindromes = nums.map do |num|
+      palindrome = num.to_s.reverse.to_i
+      sum = num + palindrome
+      [num, palindrome, sum]
+    end
+
+    all = []
+
+    palindromes.each do |palindrome|
+      if palindrome[2] >= 1000
+        all << palindrome[0]
+      end
+    end
+
+    return all.first(25)
+  end
+end
