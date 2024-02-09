44c79e1fdbaa99f94ef3dd2be3ae0cf5674eb222
Garrett Gregor <garrett.gregor@gmail.com>
2024-02-09 15:26:51 -0700

    test: get in with nested and unnested directions

---
+++ get_in/spec/get_in_spec.rb
@@ -0,0 +1,49 @@
+require "get_in"
+
+RSpec.describe GetIn do
+  let!(:instance) { GetIn.new }
+
+  describe "#intialize" do
+    it "exists" do
+      expect(instance).to be_a(GetIn)
+    end
+  end
+
+  describe "#get_in" do
+    it "can get inside of a hash using directions" do
+      data = { an_outer: "hash",
+        with_another: {
+          inner: "hash",
+          and_another: {
+            inside: "that"
+          }
+        }
+      }
+
+      directions = [:with_another, :and_another, :inside]
+
+      result = "that"
+
+      expect(instance.getin(data, directions)).to eq(result)
+    end
+
+    it "can get inside of a hash using nested directions" do
+      data = { an_outer: "hash",
+        with_another: {
+          inner: "hash",
+          and_another: {
+            inside: "that"
+          }
+        }
+      }
+
+      directions = [:with_another, [:inner, [:and_another, :inside]]]
+      # with another > inner
+      # with another > inner > and another > inside
+
+      result = ["hash", "that"]
+
+      expect(instance.getin(data, directions)).to eq(result)
+    end
+  end
+end
